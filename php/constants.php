7d75a64df0a32283ce6c0a8c251f560c0f013c32
Rupioe <rupioe@protonmail.com>
2023-05-31 13:44:50 +0200

    Petites modif

---
+++ php/constants.php
@@ -0,0 +1,7 @@
+<?php
+  // Database constants.
+  define('DB_USER', 'comweb_tp');
+  define('DB_PASSWORD', 'pt_bewmoc_isen29');
+  define('DB_NAME', 'comweb_tp');
+  define('DB_SERVER', 'localhost');
+?>
