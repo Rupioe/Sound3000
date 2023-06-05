8812c7a9aba9439b83de1379b2acf844dc3bc9f9
Y0uenn <youennlehenaff@gmail.com>
2023-06-05 15:48:34 +0200

    update

--- php/test.php
+++ php/test.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-<link href="../css/base.css" rel="stylesheet" type="text/css"/>
+<link href="../css/test.css" rel="stylesheet" type="text/css"/>
 <link href="../css/header_footer.css" rel="stylesheet" type="text/css"/>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,12 +18,24 @@
 
 <!-- ####################################################### -->
 
-<div class="imgBox">
-    <img src="../resources/images/compte/compte.png">
+<div class="player">
+    <div class="imgBox">
+        <img src="../resources/images/compte/compte.png">
+    </div>
+    <audio controls>
+        <source src="../resources/sounds/club.mp3" type="audio/mp3">
+    </audio>
 </div>
 
 
+<div class="music-button"> 
+    <img src="../html/image/rectangle.jpg">
+    <img src="../html/image/play.png">
+    <img src="../html/image/music.png">
+    <img src="../html/image/add.png">
 
+
+</div>
 
 
 
