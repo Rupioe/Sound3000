d32bf879de2a935fd6f8e5e09016a7c365839e3b
Y0uenn <youennlehenaff@gmail.com>
2023-06-05 21:54:35 +0200

    album

--- php/album.php
+++ php/album.php
@@ -11,7 +11,7 @@
 <title>Sound 3000</title>
 </head>
 <body>
-<?php include "../html/header.html"; 
+<?php include "../php/header.php"; 
       include "./PasDeTokenPasDeChocolat.php";
 ?>
 
@@ -22,35 +22,136 @@
         </div>
 
         <div class="element">                
-            Album :  <s></s>          <br>
-            Name : <s></s>          <br>
-            Creation :  <s></s>          <br>
-            Style :  <s></s>          <br>
+            Album<s></s>          <br>
+            Name<s></s>          <br>
+            Creation<s></s>          <br>
+            Style<s></s>          <br>
         
         </div>
     
-        <div class="music"><h2>music : </h2></div>
-        <li>
-        <div class="card-music">
-            <div class="card-music-description">
-                <div class="card-music-description--cover">
-                    <img src="../html/image/music.png">
+        <!-- <div class="music"><h2>music : </h2></div>  -->
+        <ol class="list-card-music">   
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="card-music-description--title--artiste">
-                    <h3>titre de musique</h3>
-                    <h4>Nom artiste</h4>
+            </li>
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="card-music-actions">
-                <ul>
-                    <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
-                    <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
-                    <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
-                </ul>
-            </div>
-        </div>
-    </li>
-    
+            </li>
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="card-music">
+                    <div class="card-music-description">
+                        <div class="card-music-description--cover">
+                            <img src="../html/image/music.png">
+                        </div>
+                        <div class="card-music-description--title--artiste">
+                            <h3>titre de musique<s></s></h3>
+                            <h4>Nom artiste<s></s></h4>
+                        </div>
+                    </div>
+                    <div class="card-music-actions">
+                        <ul>
+                            <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
+                            <li><a href="" class="add-favorite"><img src="../html/image/heart.png"></a></li>
+                            <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+        </ol>
     </div>
 
     <section>
