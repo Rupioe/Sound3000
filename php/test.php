88162538ab0e4abd796060e5fcfbbd2d808cb674
Y0uenn <youennlehenaff@gmail.com>
2023-06-05 14:56:32 +0200

    resources sound

---
+++ php/test.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link href="../css/base.css" rel="stylesheet" type="text/css"/>
+<link href="../css/header_footer.css" rel="stylesheet" type="text/css"/>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+<title>Sound 3000</title>
+</head>
+
+<body>
+ <?php include "../html/header.html" ?>
+
+
+<!-- ####################################################### -->
+
+<div class="imgBox">
+    <img src="../resources/images/compte/compte.png">
+</div>
+
+
+
+
+
+
+
+
+<!-- ####################################################### -->
+
+<?php include "../html/footer.html" ?>
+</body>
