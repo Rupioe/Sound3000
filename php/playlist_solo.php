4ea7fc28427324d04827bf36d92d9d52e6fd6648
Y0uenn <youennlehenaff@gmail.com>
2023-06-06 18:31:54 +0200

    Update playlist_solo.php

--- php/playlist_solo.php
+++ php/playlist_solo.php
@@ -3,6 +3,9 @@
 
 <head>
 <link href="../css/album.css" rel="stylesheet" type="text/css"/>
+<link href="../css/history.css" rel="stylesheet" type="text/css"/>
+<link href="../css/header_footer.css" rel="stylesheet" type="text/css"/>
+<link href="../css/pop-up.css" rel="stylesheet" type="text/css"/>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -29,6 +32,28 @@
         </div>
 
         <div class="music"><h2>music : </h2></div>
+        <ol class="list-card-music">
+    <li>    
+        <div class="card-music">
+            <div class="card-music-description">
+                <div class="card-music-description--cover">
+                    <img src="../html/image/music.png">
+                </div>
+                <div class="card-music-description--title--artiste">
+                    <h3>titre de musique</h3>
+                    <h4>Nom artiste</h4>
+                </div>
+            </div>
+            <div class="card-music-actions">
+                <ul>
+                    <li><a href="" class="add-playlist" onclick="openPopup()"><img src="../html/image/add.png" ></a></li>
+                    <li><a href="" class="add-favorite"></a></li>
+                    <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
+                </ul>
+            </div>
+        </div>
+    </li>
+</ol>
 
     </div>
 
@@ -38,5 +63,6 @@
 </section>
 
         <?php include "../html/footer.html" ?>
+        <script src="../js/pop-up.js" defer></script>
     </body>
 </html>
