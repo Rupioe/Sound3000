12b69cdd1fa2ebb643d661a7605cce2cfcbd265a
Rupioe <rupioe@protonmail.com>
2023-06-06 18:30:12 +0200

    playlist

--- php/playlist_solo.php
+++ php/playlist_solo.php
@@ -3,137 +3,33 @@
 
 <head>
 <link href="../css/album.css" rel="stylesheet" type="text/css"/>
-<link href="../css/header_footer.css" rel="stylesheet" type="text/css"/>
-
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
 <title>Sound 3000</title>
-<script src="../js/heart.js" defer></script>
-<script src="../js/ajax_favorites.js" defer></script>
 </head>
 <body>
-<?php include "../html/header.html"; 
+<?php include "../html/header.html";
       include "./PasDeTokenPasDeChocolat.php";
-
-    // Désactiver l'affichage des erreurs
-    error_reporting(0);
-    ini_set('display_errors', 0);
- 
-
-    try
-    {
-        $request = 'SELECT album.*, artiste.pseudo, artiste.id AS artiste_id  FROM album JOIN discographie ON album.id = discographie.id JOIN artiste ON discographie.id_artiste = artiste.id WHERE album.id ="'.$_GET['id'].'"';
-        $statement = $dbCnx->prepare($request);
-        $statement->execute();
-        $result = $statement->fetchAll();
-    }
-    catch (PDOException $exception)
-    {
-        error_log('Request error: '.$exception->getMessage());
-    }
-    foreach ( $result as $ligne){ 
-	        $titreGet = $ligne['titre'];
-            $image = $ligne['chemin_image'];
-            $date = $ligne['date_parution'];
-            $pseudo = $ligne['pseudo'];
-            $style = $ligne['style'];
-    }
 ?>
 
     <div class="flex">
 
         <div class="image">
-            <img src="<?php echo $image ?>">
+            <img src="../resources/images/album/album.png">
         </div>
 
-        <div class="element">                
-            <?php echo $titreGet ?>          <br>
-            <?php echo '<a href="./artiste.php?id='.$ligne['artiste_id'].'">'.$pseudo.'</a>'?>         <br>
-            <?php echo $date ?>          <br>
-            <?php echo $style ?>          <br>
-        
+        <div class="element">
+            Album :  <s></s>          <br>
+            Name : <s></s>          <br>
+            Creation :  <s></s>          <br>
+            Style :  <s></s>          <br>
+
         </div>
-    
-        <!-- <div class="music"><h2>music : </h2></div>  -->
-        <ol class="list-card-music">   
 
-        <?php
-    try
-    {
-        $request = 'SELECT * FROM morceau WHERE id_album ='.$_GET['id'];
-        $statement = $dbCnx->prepare($request);
-        $statement->execute();
-        $result = $statement->fetchAll();
-    }
-    catch (PDOException $exception)
-    {
-        error_log('Request error: '.$exception->getMessage());
-    }
-    foreach ( $result as $ligne){ 
-        $musique = $ligne['chemin_musique'];
-        $idSon = $ligne['id'];
-        //<!-- un morceau : -->
-            echo '<li>';
-                echo '<div class="card-music">';
-                    echo '<div class="card-music-description">';
-                        echo '<div class="card-music-description--cover">';
-                            echo '<img src="'.$ligne['chemin_image'].'">';
-                        echo '</div>';
-                        echo '<div class="card-music-description--title--artiste">';
-                            echo '<h3><a href="./music.php?id='.$idSon.'">'.$ligne['titre'].'</a></h3>';
-                            echo '<h4>'.floor($ligne['duree']/60).':'.($ligne['duree']%60).'</h4>';
-                        echo '</div>';
-                    echo '</div>';
-                    echo '<div class="card-music-actions">';
-                        echo '<ul>'; // On aura l'ID du morceau en parentChild du coeur cliqué
-                            echo '<li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>';
+        <div class="music"><h2>music : </h2></div>
 
-                            // SECTION FAVORIS --------------
-                            $maybeAdded = '';
-                                try // on va récuperer la liste des favoris existants pour l'utilisateur connecté afin de mettre en place les coeurs
-                                {
-                                                try // on récup d'abord l'email
-                                                {
-                                                    $request = 'SELECT email FROM compte WHERE token ="'.$_SESSION['token'].'"';
-                                                    $statement = $dbCnx->prepare($request);
-                                                    $statement->execute();
-                                                    $result = $statement->fetchAll();
-                                                }
-                                                catch (PDOException $exception)
-                                                {
-                                                    error_log('Request error: '.$exception->getMessage());
-                                                }
-                                                foreach ( $result as $ligne){ 
-                                                    $emailToken = $ligne['email'];
-                                                }
-                                    $request = 'SELECT * FROM ajouter_favoris WHERE email ="'.$emailToken.'"';
-                                    $statement = $dbCnx->prepare($request);
-                                    $statement->execute();
-                                    $result = $statement->fetchAll();
-                                }
-                                catch (PDOException $exception)
-                                {
-                                    error_log('Request error: '.$exception->getMessage());
-                                }
-                                foreach ( $result as $ligne){ 
-                                    if($ligne['id'] == $idSon) $maybeAdded = ' added';
-                                }
-                            echo '<li><a id="keur" href="" class="add-favorite '.$idSon.$maybeAdded.' '.$emailToken.'"></a></li>';
-                            // $maybeAdded sera soit "" soit " added" en fonction de si le morceau est dans la base favoris ou non 
-                            // ------------------------------
-
-                            echo '<li><a href="" class="play"><img src="../html/image/play.png"></a></li>';
-                        echo '</ul>';
-                    echo '</div>';
-                echo '</div>';
-            echo '</li>';
-    }
-        //<!-- /un morceau : -->
-        ?>
-
-
-        </ol>
     </div>
 
     <section>
@@ -142,6 +38,5 @@
 </section>
 
         <?php include "../html/footer.html" ?>
-
-    </body>    
+    </body>
 </html>
