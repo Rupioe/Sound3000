5136d9edb3b4ae9cc1712e38fa7b07fc8101c3b0
Rupioe <rupioe@protonmail.com>
2023-06-06 18:13:51 +0200

    favorites page finie

--- php/favorites.php
+++ php/favorites.php
@@ -10,44 +10,104 @@
 
 
 <title>Sound 3000</title>
+<script src="../js/heart.js" defer></script>
+<script src="../js/ajax_favorites.js" defer></script>
 </head>
 
 <body>
- <?php include "../html/header.html" ?>
-
 
 <!-- ####################################################### -->
+
+
+<?php
+
+    include "../html/header.html"; 
+      include "./PasDeTokenPasDeChocolat.php";
+
+    // Désactiver l'affichage des erreurs
+    error_reporting(0);
+    ini_set('display_errors', 0);
+    ?>
 
 <ol class="list-card-music">
-    <li>
-        <div class="card-music">
-            <div class="card-music-description">
-                <div class="card-music-description--cover">
-                    <img src="../html/image/music.png">
-                </div>
-                <div class="card-music-description--title--artiste">
-                    <h3>titre de musique</h3>
-                    <h4>Nom artiste</h4>
-                </div>
-            </div>
-            <div class="card-music-actions">
-                <ul>
-                    <li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>
-                    <li><a href="" class="add-favorite added"></a></li>
-                    <li><a href="" class="play"><img src="../html/image/play.png"></a></li>
-                </ul>
-            </div>
-        </div>
-    </li>
+
+<?php
+            try // on récup d'abord l'email
+            {
+                $request = 'SELECT email FROM compte WHERE token ="'.$_SESSION['token'].'"';
+                $statement = $dbCnx->prepare($request);
+                $statement->execute();
+                $result = $statement->fetchAll();
+            }
+            catch (PDOException $exception)
+            {
+                error_log('Request error: '.$exception->getMessage());
+            }
+            foreach ( $result as $ligne){ 
+                $emailToken = $ligne['email'];
+            }
+$statement = $dbCnx->prepare($request);
+$statement->execute();
+$result = $statement->fetchAll();
+?>
+        <?php
+    try
+    {
+        $request = 'SELECT * FROM ajouter_favoris WHERE email ="'.$emailToken.'"';
+        $statement = $dbCnx->prepare($request);
+        $statement->execute();
+        $result = $statement->fetchAll();
+    }
+    catch (PDOException $exception)
+    {
+        error_log('Request error: '.$exception->getMessage());
+    }
+    foreach ( $result as $ligne){ 
+        $idSon = $ligne['id'];
+                        try
+                        {
+                            $request = 'SELECT * FROM morceau WHERE id ='.$idSon;
+                            $statement = $dbCnx->prepare($request);
+                            $statement->execute();
+                            $result = $statement->fetchAll();
+                        }
+                        catch (PDOException $exception)
+                        {
+                            error_log('Request error: '.$exception->getMessage());
+                        }
+                        foreach ( $result as $ligneAlt){ 
+                            $musique = $ligneAlt['chemin_musique'];
+        //<!-- un morceau : -->
+            echo '<li>';
+                echo '<div class="card-music">';
+                    echo '<div class="card-music-description">';
+                        echo '<div class="card-music-description--cover">';
+                            echo '<img src="'.$ligneAlt['chemin_image'].'">';
+                        echo '</div>';
+                        echo '<div class="card-music-description--title--artiste">';
+                            echo '<h3><a href="./music.php?id='.$idSon.'">'.$ligneAlt['titre'].'</a></h3>';
+                            echo '<h4>'.floor($ligneAlt['duree']/60).':'.($ligneAlt['duree']%60).'</h4>';
+                        echo '</div>';
+                    echo '</div>';
+                    echo '<div class="card-music-actions">';
+                        echo '<ul>'; // On aura l'ID du morceau en parentChild du coeur cliqué
+                            echo '<li><a href="" class="add-playlist"><img src="../html/image/add.png"></a></li>';
+
+                            // SECTION FAVORIS --------------
+                            echo '<li><a id="keur" href="" class="add-favorite '.$idSon.' added '.$emailToken.'"></a></li>';
+                            // $maybeAdded sera soit "" soit " added" en fonction de si le morceau est dans la base favoris ou non 
+                            // ------------------------------
+
+                            echo '<li><a href="" class="play"><img src="../html/image/play.png"></a></li>';
+                        echo '</ul>';
+                    echo '</div>';
+                echo '</div>';
+            echo '</li>';
+    }
+}
+        //<!-- /un morceau : -->
+        ?>
 </ol>
-
-
-
-
-
-<script src="../js/heart.js" defer></script>
-
-<!-- ####################################################### -->
 
 <?php include "../html/footer.html" ?>
 </body>
